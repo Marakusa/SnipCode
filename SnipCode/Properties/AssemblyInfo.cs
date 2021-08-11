2779e9b9a188253aaf350545bc45e498ed308d7a
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-08-11 07:05:30 +0300

    Added splash screen

--- SnipCode/Properties/AssemblyInfo.cs
+++ SnipCode/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SnipCode")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Markus Kannisto 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
